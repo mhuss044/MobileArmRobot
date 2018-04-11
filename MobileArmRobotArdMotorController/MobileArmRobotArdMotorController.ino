3df1b985be679339b811cf7345570302e62cef2a
mhuss044 <mhuss044@gmail.com>
2018-04-11 17:56:15 -0400

    Final demonstrated arduino motor controller

--- MobileArmRobotArdMotorController/MobileArmRobotArdMotorController.ino
+++ MobileArmRobotArdMotorController/MobileArmRobotArdMotorController.ino
@@ -25,12 +25,15 @@ translate; 50500 for 1 meter.
 #define ARM_JOINT0_HOME_POS 940		// Joint0 and Joint1 are such that arm not extended
 #define ARM_JOINT1_HOME_POS 693		// 
 #define ARM_GRIPPER_HOME_POS 435	// Gripper is in open position
+
 #define ARM_JOINT0_MIN_POS 70
 #define ARM_JOINT0_MAX_POS 960
 #define ARM_JOINT1_MIN_POS 42
 #define ARM_JOINT1_MAX_POS 986
-#define ARM_GRIPPER_MIN_POS 527	// Gripper is in open position
-#define ARM_GRIPPER_MAX_POS 870	// Gripper is in closed position
+
+#define ARM_GRIPPER_MIN_POS 705	// Gripper is in open position
+#define ARM_GRIPPER_MAX_POS 960	// Gripper is in closed position
+
 #define ARM_J0_MIN_PWM (255/5)*1
 #define ARM_J0_MAX_PWM (255/5)*3.5
 #define ARM_J1_MIN_PWM (255/5)*1
@@ -38,6 +41,15 @@ translate; 50500 for 1 meter.
 #define ARM_GRIPPER_MIN_PWM 70   //(255/5)*0.8
 #define ARM_GRIPPER_MAX_PWM 80   //(255/5)*1.33
 #define CONTROL_SYS_SS_ERROR 50		// The error after which need to engage control sys
+
+#define TRANSLATE_1METER  50500 // Encoder value after having translated 1 meter
+#define ROTATE_45DEG 25000      // Encoder value after having rotated 45 degrees
+
+#define TRANSLATE_MAX_PWM 220 
+#define TRANSLATE_MIN_PWM 180
+
+#define ROTATE_MAX_PWM 120 
+#define ROTATE_MIN_PWM 100	// not sure
 
 struct MOTOR
 {
@@ -52,15 +64,25 @@ MOTOR motor_trans = { 33, 31, 8, 2, 0, 0, 0, 0, 150, 180, 0};        // bwd/fwd
 MOTOR motor_rot = { 37, 35, 9, 3, 0, 0, 0, 0, 100, 140, 0};            // rot
 // Arm Motors
 MOTOR motor_armJoint0 = { 41, 39, 4, A11, 0, ARM_JOINT0_HOME_POS, ARM_JOINT0_MAX_POS, ARM_JOINT0_MIN_POS, ARM_J0_MIN_PWM, ARM_J0_MAX_PWM, 0};
-MOTOR motor_armJoint1 = { 49, 47, 6, A12, 0, ARM_JOINT1_HOME_POS, ARM_JOINT0_MAX_POS, ARM_JOINT0_MIN_POS, ARM_J1_MIN_PWM, ARM_J1_MAX_PWM, 0};
+MOTOR motor_armJoint1 = { 49, 47, 6, A12, 0, ARM_JOINT1_HOME_POS, ARM_JOINT0_MAX_POS, ARM_JOINT0_MIN_POS, ARM_J1_MIN_PWM, ARM_J1_MAX_PWM, 0};//110
 MOTOR motor_gripper = { 45, 43, 5, A10, 0, ARM_GRIPPER_HOME_POS, ARM_GRIPPER_MAX_POS, ARM_GRIPPER_MIN_POS, ARM_GRIPPER_MIN_PWM, ARM_GRIPPER_MAX_PWM, 0};         	   // gripper motor
 
-int operatingMode = 3;		// Determines what instructions to execute
+int operatingMode = 0;		// Determines what instructions to execute
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
 int joint0Pos = 700, joint1Pos = 400;
 unsigned long int curtime = 0;
 unsigned long int lastTime= 0;
+unsigned long int translationEncoder = 0, rotationEncoder = 0;      // Summation of encoder change, trans; 50500 = 1m, rot; 25000 = 45deg
+
+void incTransEncoder()
+{
+  translationEncoder++;
+}
+void incRotEncoder()
+{
+  rotationEncoder++;
+}
 
 int updatePID(int sensorValue, int target)		// Compute PWM pulse from error
 {
@@ -210,7 +232,7 @@ int motorControl(MOTOR a)	// returns 1 if control engaged, 0 target reached
 	}
 }
 
-void gripperControl(MOTOR *a, bool open)	// open = true;mv gripper to max pos
+int gripperControl(MOTOR *a, bool open)	// open = true;mv gripper to max pos, return 0; Acquiring target, 1: target found
 {						// Check error tolerance; when was at 20, saw motor tried to push past open..
 						// add feature to close at certain dist from closed
 	int PWM = 0;
@@ -223,59 +245,78 @@ void gripperControl(MOTOR *a, bool open)	// open = true;mv gripper to max pos
 	switch(open)
 	{
 	case true:	// move gripper to min pos
-		if(abs(sensorValue - ARM_GRIPPER_MIN_POS) > 100)
+		if(abs(sensorValue - ARM_GRIPPER_MIN_POS) > 80)
     {
 			if(sensorValue > ARM_GRIPPER_MIN_POS)	// shaft is past the open position  		
 			{
 				a->curPWM = ARM_GRIPPER_MIN_PWM + (ARM_GRIPPER_MAX_PWM - ARM_GRIPPER_MIN_PWM)*(abs(ARM_GRIPPER_MIN_POS-sensorValue)/1023.0);
 
+        if(a->curPWM > 240)     // Constrain
+          a->curPWM = 240;
+        else if(PWM < 0)
+          a->curPWM = 0;
+
 				motorBwd(*a, a->curPWM); // moves toward open pos
+        
+        return 0;               // Indicate acquiring target
 			}
     }
     else
     {
-      motorBwd(*a, 0); // moves toward open pos
+      motorBwd(*a, 0); // Stop
+
+      return 1;               // Indicate target reached
     }
 	break;
 	case false:
-		if(abs(sensorValue - ARM_GRIPPER_MAX_POS) > 100)
+		if(abs(sensorValue - ARM_GRIPPER_MAX_POS) > 80)
     {
 			if(sensorValue < ARM_GRIPPER_MAX_POS)	// shaft is below the closed position  		
 			{
 				a->curPWM = ARM_GRIPPER_MIN_PWM + (ARM_GRIPPER_MAX_PWM - ARM_GRIPPER_MIN_PWM)*(abs(ARM_GRIPPER_MAX_POS-sensorValue)/1023.0);
 
-				motorFwd(*a, a->curPWM); // moves toward open pos
+        
+        if(a->curPWM > 240)     // Constrain
+          a->curPWM = 240;
+        else if(PWM < 0)
+          a->curPWM = 0;
+				
+				motorFwd(*a, a->curPWM); // moves toward closed pos
+
+        return 0;               // Indicate acquiring target
 			}
     }
     else
     {
       motorFwd(*a, 0);
+    
+      return 1;               // Indicate target reached
     }
 	break;
 	}
-
-	if(a->curPWM > 240)			// Constrain
-		a->curPWM = 240;
-	else if(PWM < 0)
-		a->curPWM = 0;
 }
 
 void setup() 
 {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600); 
+
+  pinMode(13, OUTPUT); 
   
   // Setup motor conrol I/O pins 
   // Movement:
   pinMode(motor_trans.motorIN1, OUTPUT); 
   pinMode(motor_trans.motorIN2, OUTPUT); 
   pinMode(motor_trans.motorEN, OUTPUT); 
-  pinMode(motor_trans.sensorPin, INPUT); 
+  pinMode(motor_trans.sensorPin, INPUT_PULLUP); 
+  attachInterrupt(digitalPinToInterrupt(motor_trans.sensorPin),incTransEncoder,CHANGE);
 
   pinMode(motor_rot.motorIN1, OUTPUT); 
   pinMode(motor_rot.motorIN2, OUTPUT); 
   pinMode(motor_rot.motorEN, OUTPUT); 
-  pinMode(motor_rot.sensorPin, INPUT); 
+  pinMode(motor_rot.sensorPin, INPUT_PULLUP); 
+  attachInterrupt(digitalPinToInterrupt(motor_rot.sensorPin),incRotEncoder,CHANGE);
+
   // Arm
   pinMode(motor_armJoint0.motorIN1, OUTPUT); 
   pinMode(motor_armJoint0.motorIN2, OUTPUT); 
@@ -293,38 +334,103 @@ void setup()
   pinMode(motor_gripper.sensorPin, INPUT); 
 
 }
-
+/*
+void serialEvent() 
+{
+  while (Serial.available()) 
+  {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
+*/
 bool gripperAction = false;
+int i1 = 0, i2 = 0;
+int distToTravelFactor = 0;
 
 void loop() 
-{
-	/*
-	if(Serial.available())
-	{  Serial.read()
-	*/
+{	
 	switch(operatingMode)
 	{
 		case 0:		// The robot is awaiting instructions
-		// Arm motors at home positions, platform not mving
-		// waiting for instructions: send 20 as in waiting
-		// Once receive inst, send 1 to indicate done mode0
+			// Arm motors at home positions, platform not mving
+			// waiting for instructions: send 20 as in waiting
+			// Once receive inst, send 1 to indicate done mode0
+			
+			digitalWrite(13, LOW);
+			if(Serial.available())
+			{  
+				distToTravelFactor = Serial.parseInt();
+				if(distToTravelFactor == 2)	// waits 1sec for an int	
+					{
+					  //Serial.print("Received 3\n");
+					   //Serial.write("Done\n");
+					   digitalWrite(13, HIGH);
+             while(!gripperControl(&motor_gripper, true))
+					   delay(2000);
+					   operatingMode = 1;
+					// Open gripper in preparation for coming grasp
+					}
+				else if(distToTravelFactor == 1)
+				{
+					digitalWrite(13, HIGH);
+					while(!gripperControl(&motor_gripper, true))
+					delay(2000);
+					operatingMode = 1;
+					// Open gripper in preparation for coming grasp
+					gripperControl(&motor_gripper, true);
+				} 
+			}
 		break;
 		case 1:		// Received instructions, move to target
-		// Execute movement commands to mv to loc	
-		// moving: send 21; indicates 2: operation incomplete, 1: operating in mode 1
-		// if(recvd == 11) go to loc 1
-		// elseif (recv == 12) go to loc 2
-		// done move, send 1 indicate done mode1, going to mode 2
+			// Execute movement commands to mv to loc	
+			// moving: send 21; indicates 2: operation incomplete, 1: operating in mode 1
+			// if(recvd == 11) go to loc 1
+			// elseif (recv == 12) go to loc 2
+			// done move, send 1 indicate done mode1, going to mode 2
+
+			// Move forward 1 m, when done, goto opmode2, which is rotate
+			if(translationEncoder < TRANSLATE_1METER*distToTravelFactor)
+			{
+				motorFwd(motor_trans, TRANSLATE_MAX_PWM);
+			}
+			else
+			{
+				motorFwd(motor_trans, 0);
+       translationEncoder = 0;
+				operatingMode = 3;
+				// Detach intterupt? bcs no longer need it
+			}
+			//motorControl(motor_rot);
 		break;
 		case 2:		// Moved to in front of target, awaiting angles to place gipper
-		// Arm motors at home pos, platform not moving
-		// 
+			// Arm motors at home pos, platform not moving
+			// 
+
+			// Rotate 45 deg,when done goto opmode3, which is arm 
+			if(rotationEncoder < ROTATE_45DEG)
+			{
+				motorBwd(motor_rot, ROTATE_MAX_PWM);
+			}
+			else
+			{
+				motorBwd(motor_rot, 0);
+				operatingMode = 3;
+				// Detach intterupt? bcs no longer need it
+			}
 		break;
 		case 3:		// Received angle commands
-		// Move arm to required angles
-		joint0Pos = 700;
-		joint1Pos = 450;    // 380 -
-   
+			// Move arm to required angles
+			joint0Pos = 720;
+			joint1Pos = 450;    // 380 -
+	   
 			motor_armJoint0.curTarget = joint0Pos;
 			motor_armJoint1.curTarget = joint1Pos;
 			
@@ -334,56 +440,98 @@ void loop()
 			updateMotorShaftPos(&motor_armJoint1);
 			//updateMotorShaftPos(motor_gripper);
 
-      Serial.print("\tPWM vals (J0, J1): ");
-      Serial.print(motor_armJoint0.curPWM); 
-      Serial.print(",");
-      Serial.println(motor_armJoint1.curPWM);
-      
-     // delay(100);
+			Serial.print("\tPWM vals (J0, J1): ");
+			Serial.print(motor_armJoint0.curPWM); 
+			Serial.print(",");
+			Serial.println(motor_armJoint1.curPWM);
+			      
+		     // delay(100);
 
-        if(moveJointToPos(&motor_armJoint1, joint1Pos))
+			if(moveJointToPos(&motor_armJoint1, joint1Pos))
+			{
+			  Serial.print("\t<---Joint 1 Target Found. Error: ");
+			  Serial.println(abs(motor_armJoint1.curTarget-motor_armJoint1.curShaftPos)); 
+
+			  if(moveJointToPos(&motor_armJoint0, joint0Pos))
+			  {
+			    Serial.print("\t<---Joint 0 Target Found. Error: ");
+			    Serial.println(abs(motor_armJoint0.curTarget-motor_armJoint0.curShaftPos)); 
+			  }
+			  else
+			  {
+			    Serial.print("\tJoint 0 Acquiring Target. Error: ");
+			    Serial.println(abs(motor_armJoint0.curTarget-motor_armJoint0.curShaftPos));
+			    //Serial.println(sensorValue);
+			  }
+			}
+			else
+			{
+			  Serial.print("\tJoint 1 Acquiring Target. Error: ");
+			  Serial.println(abs(motor_armJoint1.curTarget-motor_armJoint1.curShaftPos));
+			  //Serial.println(sensorValue);
+			}  
+
+			if((abs(motor_armJoint1.curTarget-motor_armJoint1.curShaftPos) + abs(motor_armJoint0.curTarget-motor_armJoint0.curShaftPos)) < 20)
         {
-          Serial.print("\t<---Joint 1 Target Found. Error: ");
-          Serial.println(abs(motor_armJoint1.curTarget-motor_armJoint1.curShaftPos)); 
-
-          if(moveJointToPos(&motor_armJoint0, joint0Pos))
+          while(!gripperControl(&motor_gripper, false))
           {
-            Serial.print("\t<---Joint 0 Target Found. Error: ");
-            Serial.println(abs(motor_armJoint0.curTarget-motor_armJoint0.curShaftPos)); 
-            gripperAction = true;
+            //moveJointToPos(&motor_armJoint1, joint1Pos);
+            //moveJointToPos(&motor_armJoint0, joint0Pos);
+            Serial.print("\tGrip.");
+            Serial.println(abs(motor_gripper.curShaftPos - ARM_GRIPPER_MAX_POS));
+            operatingMode = 5;
+            if(abs(motor_gripper.curShaftPos - ARM_GRIPPER_MAX_POS) < 250)
+              break;
           }
-          else
-          {
-            Serial.print("\tJoint 0 Acquiring Target. Error: ");
-            Serial.println(abs(motor_armJoint0.curTarget-motor_armJoint0.curShaftPos));
-            //Serial.println(sensorValue);
-            gripperAction = false;
-          }
+          motorBwd(motor_armJoint0,40);
+          motorBwd(motor_armJoint1,50);
+          translationEncoder = 0;
+          operatingMode = 5;
         }
-        else
-        {
-          Serial.print("\tJoint 1 Acquiring Target. Error: ");
-          Serial.println(abs(motor_armJoint1.curTarget-motor_armJoint1.curShaftPos));
-          //Serial.println(sensorValue);
-          gripperAction = false;
-        }  
-
-        if((abs(motor_armJoint1.curTarget-motor_armJoint1.curShaftPos) + abs(motor_armJoint0.curTarget-motor_armJoint0.curShaftPos)) < 20)
-          gripperControl(&motor_gripper, false);
-        
-      
-			//motorControl(motor_trans);
-			//motorControl(motor_rot);
-			//motorControl(motor_armJoint0);
-			//motorControl(motor_armJoint1);
-			//motorControl(motor_gripper);
+      /*
+			  if(gripperControl(&motor_gripper, false))   // if true, target found
+				{
+					operatingMode = 5;	// go back to user
+					translationEncoder = 0; 
+					// Gripped, now slowly disengage arm..dont want platform and arm on: >3amps
+					motorBwd(motor_armJoint0,0);
+					motorBwd(motor_armJoint1,50);
+					
+			    		//gripperAction = !gripperAction;                   // change target
+				}
+       */
 			
 			delay(20);	// Not neccessary to control evry iteration	
 		break;
 		case 4:		// Gripper at position after 3
-		// Grip
+			// Grip
+
+			// Rotate 45 deg,when done goto opmode3, which is arm 
+			if(rotationEncoder < ROTATE_45DEG)
+			{
+				motorFwd(motor_rot, ROTATE_MAX_PWM);
+			}
+			else
+			{
+				motorFwd(motor_rot, 0);
+				//translationEncoder = 0;
+				rotationEncoder = 0;
+				operatingMode = 5;
+				// Detach intterupt? bcs no longer need it
+			}
 		break;
 		case 5:		// Obj gripped. move back to usr
+	     
+			// Move forward 1 m, when done, goto opmode2, which is rotate
+			if(translationEncoder < TRANSLATE_1METER*distToTravelFactor)
+			{
+				motorBwd(motor_trans, TRANSLATE_MAX_PWM);
+			}
+			else
+			{
+				motorBwd(motor_trans, 0);
+				// Detach intterupt? bcs no longer need it
+			}
 		break;
 	}
  }
--- server.py
+++ server.py
@@ -7,21 +7,45 @@
 import socket
 import time
 import serial
+import RPi.GPIO as GPIO
 
 #   create socket obj
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #   get local machine name
 #host = socket.gethostname()
-host = '192.168.1.165'
+# 192.168.0.100
+host = '192.168.0.100'
 port = 12345
 
 #   bind port
-serversocket.bind((host, port))
+#serversocket.bind((host, port))
 
 #queue up to 5 requests
-serversocket.listen(5)
+#serversocket.listen(5)
 
+GPIO.setmode(GPIO.BCM)
+
+'''
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+'''
+
+GPIO.setup(20, GPIO.IN) # pin 20 as input
+ser = serial.Serial('/dev/ttyACM0', 9600)   # open serial con with ard
+time.sleep(2)       # since after open serial, ard resets, wait before send
+    
 while True:
+    if GPIO.input(20)   # high on pin 20, go
+        ser.write('3')
+        while True:
+                print ser.readline()
+
+
+    # while True:
+    '''
     #   establish connection
     s,addr = serversocket.accept()
     
@@ -35,8 +59,43 @@ while True:
     print(reply.decode())
 
     s.close()
+    '''
+    #print(serial.tools.list_ports)
+    # if button1, then move to room 1
+    # if button2, then move to room 2
 
-    ser = serial.Serial('/dev/ttyACM0', 9600)
-    # ser.write('3')
-    while true:
-            print(ser.readline())
+''' 
+		Kitchen = GPIO.input(18)
+		flag = False
+		if Kitchen == False:
+			Kobj1 = GPIO.input(23)
+			Kobj2 = GPIO.input(24)
+			if Kobj1 == False:
+				ans = '0'
+				print(ans)
+				time.sleep(0.2)
+				flag = True
+			if Kobj2 == False:
+				ans = '1'
+				print(ans)
+				time.sleep(0.2)
+				flag = True
+		
+		Bedroom = GPIO.input(25)
+		if Bedroom == False:
+			Bobj1 = GPIO.input(12)
+			Bobj2 = GPIO.input(16)
+			if Bobj1 == False:
+				ans = '2'
+				print(ans)
+				time.sleep(0.2)
+				flag = True
+				
+			Bobj2 = GPIO.input(16)
+			if Kobj2 == False:
+				ans = '3'
+				print(ans)
+				time.sleep(0.2)
+				flag = True
+
+'''
